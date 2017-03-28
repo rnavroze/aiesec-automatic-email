6019a838ce1783083f27591f4435cf83471f8f72
Raihan Navroze <raihan@aiesec.net>
2017-03-28 17:51:15 +0530

    Commented upload.php

--- upload.php
+++ upload.php
@@ -1,7 +1,32 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: rnavr
- * Date: 28-03-2017
- * Time: 03:43 PM
- */
+// Some POST data
+$entity_id = $_POST['entity'];
+$status = $_POST['status'];
+
+// We want to put the file into html/STATUS-ENTITY_ID.html
+$target_dir = "html/";
+$fname = "$status-$entity_id.html";
+
+$target_file = $target_dir . $fname;
+
+// Flag to check if upload was successful
+$upload_ok = true;
+$filetype = pathinfo($_FILES["html_file"]["name"], PATHINFO_EXTENSION);
+
+if (isset($_POST["submit"]))
+{
+	if ($filetype != "html")
+	{
+		echo "File is not an HTML file.";
+		$upload_ok = false;
+	}
+}
+
+// Move uploaded file to where we want it to be
+if ($upload_ok)
+{
+	if (move_uploaded_file($_FILES["html_file"]["tmp_name"], $target_file))
+		header("Location: index.php?success=true");
+	else
+		echo "An error occurred while uploading the file.";
+}
